4aae32061577d6fc2d654fa9f30b0a8f5bdef5d2
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-31 13:13:58 +0330

    The Crane model is added to the Cranes app models file.

--- Server/Cranes/models.py
+++ Server/Cranes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Crane(models.Model):
+    name = models.CharField(verbose_name="نام")
+    slug = models.SlugField(verbose_name="اسلاگ", unique=True)
+
+    image = models.FileField(verbose_name="تصویر", upload_to="Cranes/images/")
+
+    conetnt = models.TextField(verbose_name="محتوا", null=True, blank=True)
+
+
+    class Meta:
+        verbose_name = "جرثقیل"
+        verbose_name_plural = "جرثقیل ها"
+
+    
+    def __str__(self):
+        return self.name
