817822d7971f3d46e1a2576af8e13b9a7e2cbbd0
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-31 14:53:24 +0330

    The Category model is added to the blogs app.

--- Server/Blogs/models.py
+++ Server/Blogs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Category(models.Model):
+    name = models.CharField(verbose_name="نام", max_length=255)
+    slug = models.SlugField(verbose_name="اسلاگ", max_length=255, unique=True)
+
+    image = models.ImageField(upload_to="Blogs/Category/images/")
+
+    class Meta:
+        verbose_name = "دسته بندی"
+        verbose_name_plural = "دسته بندی ها"
+    
+    def __str__(self):
+        return self.name
