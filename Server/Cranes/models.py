733efed5a0c769242d9a46be3103c8eb6917437f
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-31 13:33:01 +0330

    The CraneContent model is added to the project.

--- Server/Cranes/models.py
+++ Server/Cranes/models.py
@@ -19,3 +19,39 @@ class Crane(models.Model):
     
     def __str__(self):
         return self.name
+    
+
+
+class CraneContent(models.Model):
+
+    crane = models.ForeignKey(
+        Crane,
+        on_delete=models.CASCADE,
+        verbose_name="جرثقیل",
+        related_name="crane_contents"
+    )
+
+    title = models.CharField(
+        verbose_name="عنوان", 
+        blank=True, null=True
+    )
+
+    content = models.TextField(
+        verbose_name="محتوا",
+        blank=True, null=True
+    )
+
+    image = models.FileField(
+        upload_to="Cranes/Content/images/",
+        verbose_name="تصویر",
+        null=True, blank=True
+    )
+
+
+    class Meta:
+        verbose_name = "محتوا"
+        verbose_name_plural = "محتوا ها"
+
+    
+    def __str__(self):
+        return f'{self.title}--{self.crane.name}'
