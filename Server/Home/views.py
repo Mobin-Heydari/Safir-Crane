9c5e10e2b23d1555d2b61f098ded67dacea70817
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-01 19:32:56 +0330

    The blogs and cranes querysets are added to the home view.

--- Server/Home/views.py
+++ Server/Home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 
+from Blogs.models import Blog
+from Cranes.models import Crane
+
+
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'Home/index.html')
+        blogs = Blog.objects.filter(status='P').order_by('-published_date')[:6]
+        cranes = Crane.objects.all()[:6]
+        return render(request, 'Home/index.html', {'cranes': cranes, 'blogs': blogs})
