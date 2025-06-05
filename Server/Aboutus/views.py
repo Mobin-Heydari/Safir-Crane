cc769589d2039b7128a73e03994cf30d971da91d
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-05 11:11:43 +0330

    The abou-us page view and url added.

--- Server/Aboutus/views.py
+++ Server/Aboutus/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+
+class AboutUsView(View):
+    def get(self, request):
+        return render(request, 'Aboutus/about-us.html')
--- Server/Server/urls.py
+++ Server/Server/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('blogs/', include('Blogs.urls', namespace="blogs")),
     path('cranes/', include('Cranes.urls', namespace="cranes")),
     path('contact-us/', include('Contacts.urls', namespace="contacts")),
+    path('about-us/', include('Aboutus.urls', namespace="about-us")),
 ]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
