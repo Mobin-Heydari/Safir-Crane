e3f1b2044e9fce45ee74185f0ade01ee386f8be8
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-05 15:08:59 +0330

    The blog category filtering systme and most viewed blogs added.

--- Server/Blogs/urls.py
+++ Server/Blogs/urls.py
@@ -9,5 +9,6 @@ app_name = "Blogs"
 
 urlpatterns = [
     path('list/', views.BlogsListView.as_view(), name="blog-list"),
+    path('list/<slug:category_slug>/', views.BlogsListView.as_view(), name="blog-list-category"),
     path('detail/<slug:slug>/', views.BlogsListView.as_view(), name="blog-detail")
 ]
--- Server/Blogs/views.py
+++ Server/Blogs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
-from .models  import Blog, BlogComment, BlogContent
+from .models  import Blog, Category, BlogComment, BlogContent
 
 
 
@@ -9,9 +9,13 @@ from .models  import Blog, BlogComment, BlogContent
 
 class BlogsListView(View):
 
-    def get(self, request):
+    def get(self, request, category_slug=None):
         blogs = Blog.objects.filter(status="P")
-        return render(request, 'Blogs/blog-list.html', {'blogs': blogs})
+        most_viewed_blogs = blogs.order_by('-views')[:3]
+        categories = Category.objects.all()
+        if category_slug is not None:
+            blogs.filter(category__slug=category_slug)
+        return render(request, 'Blogs/blog-list.html', {'blogs': blogs, 'most_viewed': most_viewed_blogs, 'categories': categories})
 
 
 
