703ea3c4b0dbc7b81e3dee235e28de8e68ed76ad
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-10 16:15:56 +0330

    The CraneRequestFrom is added to the Cranes forms.py file.

---
+++ Server/Cranes/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.core import validators
+
+
+
+class CraneRequestForm(forms.Form):
+    title = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control',
+        'placeholder' : 'عنوان',
+        'id' : 'title'
+    }))
+
+    f_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control text-left',
+        'placeholder' : 'نام',
+        'id' : 'name'
+    }))
+
+    l_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control',
+        'placeholder' : 'نام خانوادگی',
+        'id' : 'name'
+    }))
+    
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class' : 'form-control text-left',
+        'placeholder' : 'ایمیل',
+        'id' : 'email'
+    }), validators=[validators.EmailValidator])
+    
+    phone = forms.CharField(widget=forms.TextInput(attrs={
+        'class' : 'form-control',
+        'placeholder' : 'شماره تماس',
+        'id' : 'number'
+    }), validators=[validators.MaxLengthValidator(12)])
+    
+    content = forms.CharField(widget=forms.Textarea(attrs={
+        'class' : 'form-control',
+        'placeholder' : 'توضیحات',
+        'id' : 'content'
+    }))
