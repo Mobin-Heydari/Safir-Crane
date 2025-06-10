76e68cce510ce32874d397296e4c40d309a478d2
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-10 16:42:31 +0330

    The form action post is available.

--- Server/Cranes/views.py
+++ Server/Cranes/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.core.paginator import Paginator
 
 from .forms import CraneRequestForm
 
-from .models  import Crane, CraneContent, CraneImages, CraneRequest
+from .models  import Crane, CraneRequest
 
 
 
@@ -30,3 +30,25 @@ class CraneDetailView(View):
         crane = get_object_or_404(Crane, slug=slug)
         other_cranes = Crane.objects.all().order_by('?')
         return render(request, 'Cranes/crane-detail.html', {'crane': crane, 'other_cranes': other_cranes, 'form': form})
+    
+    def post(self, request, slug):
+        form = CraneRequestForm(request.POST)
+        
+        if form.is_valid():
+            cd = form.cleaned_data
+            crane = get_object_or_404(Crane, slug=slug)
+            
+            instance = CraneRequest.objects.create(
+                crane=crane,
+                f_name=cd['f_name'],
+                l_name=cd['l_name'],
+                title=cd['title'],
+                phone=cd['phone'],
+                email=cd['email'],
+                content=cd['content']
+            )
+            instance.save()
+
+            return redirect('cranes:crane-detail', slug)
+        
+        return render(request, 'cranes/crane-detail.html', {'crane': instance, 'form': form})
