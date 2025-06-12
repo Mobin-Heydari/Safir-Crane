0aad47866c8db37db20fcbbe0a289d4f353906fd
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-12 14:53:54 +0330

    The contact form is added to the main page and available for users.

--- Server/Home/views.py
+++ Server/Home/views.py
@@ -3,6 +3,7 @@ from django.views import View
 
 from Blogs.models import Blog
 from Cranes.models import Crane
+from Contacts.forms import ContactsForm
 
 
 
@@ -11,4 +12,5 @@ class HomeView(View):
     def get(self, request):
         blogs = Blog.objects.filter(status='P').order_by('-published_date')[:6]
         cranes = Crane.objects.all()[:6]
-        return render(request, 'Home/index.html', {'cranes': cranes, 'blogs': blogs})
+        form = ContactsForm()
+        return render(request, 'Home/index.html', {'cranes': cranes, 'blogs': blogs, 'form': form})
